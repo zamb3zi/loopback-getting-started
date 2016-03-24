c98cbf88d29fa5fa478fee070527847e0de352c1
John McLaughlin <johnlock+freelancing@gmail.com>
2016-03-24 11:15:19 +0700

    Added gulp and webpack build process

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
+// install source-map support so we get mapped stack traces.
+require('source-map-support').install();
+
 var loopback = require('loopback');
-var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
@@ -18,10 +20,22 @@ app.start = function() {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
-
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+console.log('Executing boot instructions...');
+// instructions are provided by an explicit webpack resolve
+// alias (see gulpfile.js).
+var ins = require('boot-instructions.json');
+// install the external dynamic configuration.
+ins.config = require('./config.json');
+ins.dataSources = require('./datasources.json');
+var execute = require('loopback-boot/lib/executor');
+execute(app, ins, function (err) {
+    if (err) {
+        console.error(`Boot error: ${err}`);
+        throw err;
+    }
+    console.log('Starting server...');
+    // NOTE/TODO: the require.main === module check fails here under webpack
+    // so we're not doing it.
+    var server = app.start();
 });
+console.log('Done.');
